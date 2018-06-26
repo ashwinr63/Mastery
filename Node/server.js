32ace6c9164e332c65476d3b3b44a4b6572b2f72
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2018-06-26 23:53:28 +0530

    as
    Signed-off-by: Ashwin <8831182+ashwinr63@users.noreply.github.com>

--- Node/server.js
+++ Node/server.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.get('/', (req, res) => {
+	res.send("getting root")
+});
+
+app.get('/profile', (req, res) => {
+	res.send("getting profile")
+});
+
+
+app.post('/profile', (req, res) => {
+	const user = {
+		Name: 'James',
+		Hobby: 'BasketBall'
+	}
+	res.send(user);
+});
+
+
 app.listen(3000);
+
